e737167c5229ca4183217950e07a713d952ae728
Geetesh kumar Jha <jhageetesh1995@gmail.com>
2018-10-14 01:59:35 +0530

    trying git

---
+++ Vidly/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+namespace Vidly.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Customer List shouls be displayed here";
+            List<Customer> customerList = new List<Customer>
+            {
+                new Customer(){Name= "Geetesh",Id=1},
+                new Customer(){Name= "Vishva",Id=2},
+                new Customer(){Name= "Bhaskar",Id=3}
+            };
+            return View(customerList);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
